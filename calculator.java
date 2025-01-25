68899ccbb32c170eaeff5098c6c21158b65417ce
lubaanah <lubaanah@gmail.com>
2025-01-25 12:37:43 +0530

    first commit

---
+++ calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class biotech {
+void add (int a, int b)
+{
+System.out.println("sum :"+ (a+b));
+}
+}
+
+class computer extends biotech 
+{
+void sub(int a, int b)
+{
+System.out.println("sub :" +(a-b));
+}
+}
+
+class civil extends biotech {
+void multiple (int a, int b)
+{
+System.out.println("multiple :" +(a*b));
+}
+}
+
+class calculator {
+
+public static void main(String args[])
+{
+Scanner scan = new Scanner(System.in);
+System.out.println("enter value of a :");
+int a = scan.nextInt();
+System.out.println("enter value of b:");
+int b = scan.nextInt();
+computer cs = new computer();
+civil eng = new civil();
+cs.add(a,b);
+cs.sub(a,b);
+eng.multiple(a,b);
+}
+}
